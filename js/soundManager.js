d66f873695f4779fd781fc0968f0e3c0ca3885cd
Nimira43 <n.rabone72@aol.com>
2025-11-01 13:33:22 +0000

    SoundManager constructor

---
+++ js/soundManager.js
@@ -0,0 +1,20 @@
+export class SoundManager {
+  constructor() {
+    this.audioElements = new Map()
+    this.isPlaying = false
+  }
+
+  loadSound() {}
+
+  playSound() {}
+
+  pauseSound() {}
+
+  setVolume() {}
+
+  playAll() {}
+
+  pauseAll() {}
+
+  stopAll() {}
+}
