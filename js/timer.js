f4dd1753c69897952483aad3c9c0b70b965464c4
Nimira43 <n.rabone72@aol.com>
2025-11-01 13:17:07 +0000

    Timer constructor

--- js/timer.js
+++ js/timer.js
@@ -1,5 +1,12 @@
 export class Timer {
-  constructor() {}
+  constructor(onComplete, onTick) {
+    this.duration = 0
+    this.remaining = 0
+    this.intervalId = null
+    this.onComplete = onComplete
+    this.onTick = onTick
+    this.isRunning = false
+  }
 
   start() {}
 
@@ -9,5 +16,5 @@ export class Timer {
 
   updateDisplay() {}
 
-  
+
 }
