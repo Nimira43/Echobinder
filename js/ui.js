3f0342d173d1cd8530f989ffae064a0fa84d5198
Nimira43 <n.rabone72@aol.com>
2025-11-01 13:12:28 +0000

    Outlining the UI class

---
+++ js/ui.js
@@ -0,0 +1,33 @@
+export class UI {
+  constructor() {}
+
+  init() {}
+
+  createSoundCard() {}
+
+  createCustomPresetButton() {}
+
+  renderSoundCards() {}
+
+  updateSoundPlayButton () {}
+
+  updateVolumeDisplay() {}
+
+  updateMainPlayButton() {}
+
+  resetUI() {}
+
+  showModal() {}
+
+  hideModal() {}
+
+  addCustomPreset() {}
+
+  setActivePreset() {}
+
+  removeCustomPreset() {}
+
+  updateTimerDisplay() {}
+
+  toggleTheme() {}
+}
