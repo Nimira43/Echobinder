4ccde5338103f09d4a35d205c8137933694664e4
Nimira43 <n.rabone72@aol.com>
2025-11-02 07:25:03 +0000

    EchoBinder constructor

--- js/main.js
+++ js/main.js
@@ -3,3 +3,15 @@ import { sounds, defaultPresets } from './soundData.js'
 import { SoundManager } from './soundManager.js'
 import { UI } from './ui.js'
 import { Timer } from './timer.js'
+
+class EchoBinder {
+  constructor() {
+    this.soundManager = new SoundManager()
+    this.ui = new UI()
+    this.presetManager = new PresetManager()
+    this.timer = new Timer()
+    this.currentSoundDtate = {}
+    this.masterVolume = 100
+    this.isInitialised = false
+  }
+}
